13dd91094b6c32ea5adb478b599a2db863988846
KKirimi <keithkirimi.kk@gmail.com>
2023-08-23 12:47:59 -0700

    Simple shell

---
+++ 5_simple_shell.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * _eputs - displays input string
+ * @str: string to print
+ * Return: Null
+ */
+
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - write the char c to stderr
+ * @c: char to print
+ * Return: return 1 if successful
+ * On error, return -1
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - input the char c to given fd
+ * @c: the char to print
+ * @fd: fd to write to
+ * Return: 1 if succesful
+ * On error, return -1 if successful
+ */
+
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - display an input string
+ * @str: string to print
+ * @fd: filedescriptor to write to
+ * Return: number of characters put
+ */
+
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
