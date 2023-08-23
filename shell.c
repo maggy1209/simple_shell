fc8f8481a5f468c8825e0d11a164ba107c23cdf7
maggy1209 <otienom807@gmail.com>
2023-08-23 10:47:43 -0700

    updated file

--- shell.c
+++ shell.c
@@ -1,15 +1,28 @@
-#include "shell.c"
-
-int main(int ac, char **argv)
- {
+#include "shell.h"
+int main()
+{
+  char cmd[MAX_CMD_LEN];
+  int status;
   char *prompt = "(Eshell) $ ";
   char *lineptr;
   size_t n = 0;
-
-  printf("%s", prompt);
-  getline(&lineptr, &n, stdin);
-
-  free(lineptr);
+  while (1) {
+    printf("sheep_shell>");
+    fgets(cmd, MAX_CMD_LEN, stdin);
+// Check for end of file
+    if (cmd[0] == '\n') {
+      break;
+    }
+// Fork a child process to execute the command
+    pid_t pid = fork();
+    if (pid == 0) {
+      execvp(cmd, NULL);
+      printf("Error: Could not execute command '%s'\n", cmd);
+      exit(1);
+    } else {
+      waitpid(pid, &status, 0);
+    }
+}
   return (0);
 }
 
